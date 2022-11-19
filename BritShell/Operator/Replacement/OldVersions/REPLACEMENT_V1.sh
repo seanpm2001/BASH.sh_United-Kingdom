0df891273a975851c9beb058cefaa33c2126c356
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-11-19 14:35:32 -0800

    REPLACEMENT_V1.sh
    An archive of the first version of the replacement script file for this directory set.

---
+++ BritShell/Operator/Replacement/OldVersions/REPLACEMENT_V1.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Replace the US Dollar sign with the Pound (currency) sign
+'$' -> '£'
+# File info
+# File type: BASH shell source file (*.sh)
+# File version: 1 (2022, Saturday, November 19th at 2:35 pm PST)
+# Line count (including blank lines and compiler line): 8
